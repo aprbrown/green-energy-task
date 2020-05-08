dc26aaafd778a987e85b8d67c2787b1fd8302caf
Andrew P R Brown <hello@andrewprbrown.com>
2020-05-08 14:17:51 +0100

    Cleared project to start, added ico and logos and updated metadata and manifest

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from '../../node_modules/react';
+import './App.scss';
+import Login from './login/Login'
+
+function App() {
+  return (
+      <div className='container'>
+          <Login />
+      </div>
+  );
+}
+
+export default App;
